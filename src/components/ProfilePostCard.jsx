acad65676e08c1e5f112aca5ac819ce389f76721
hoangunity <hoangdm.141196@Gmail.com>
2023-07-07 10:46:35 +0700

    Add like feature

--- src/components/ProfilePostCard.jsx
+++ src/components/ProfilePostCard.jsx
@@ -4,19 +4,45 @@ import Image from "react-bootstrap/Image";
 import Row from "react-bootstrap/Row";
 
 import { useEffect, useState } from "react";
+import jwtDecode from "jwt-decode";
+import axios from "axios";
 
 function ProfilePostCard({ content, postId }) {
   const pic = `https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg`;
-  const [likes, setLikes] = useState(0);
+  const BASE_URL = `https://twitter-api-hoangunity.sigma-school-full-stack.repl.co`;
+  const [likes, setLikes] = useState([]);
+
+  const token = localStorage.getItem("authToken");
+  const decode = jwtDecode(token);
+  const userId = decode.id;
 
   useEffect(() => {
-    fetch(
-      `https://twitter-api-hoangunity.sigma-school-full-stack.repl.co/likes/post/${postId}`
-    )
+    fetch(`${BASE_URL}/likes/post/${postId}`)
       .then((res) => res.json())
-      .then((data) => setLikes(data.length))
+      .then((data) => setLikes(data))
       .catch((err) => console.error(`Error: ${err}`));
-  }, [postId]);
+  }, [BASE_URL, postId]);
+
+  const isLiked = likes.some((like) => like.user_id === userId);
+
+  const handleLike = () => (isLiked ? removeFromLikes() : addToLikes());
+
+  const addToLikes = () => {
+    setLikes([...likes, { user_id: userId }]);
+
+    axios.post(`${BASE_URL}/likes`, {
+      user_id: userId,
+      post_id: postId,
+    });
+  };
+
+  const removeFromLikes = () => {
+    const like = likes.find((like) => like.user_id === userId);
+    if (like) {
+      axios.delete(`${BASE_URL}/likes/${like.likes_id}`);
+      setLikes(likes.filter((like) => like.user_id !== userId));
+    }
+  };
 
   return (
     <Row
@@ -40,8 +66,13 @@ function ProfilePostCard({ content, postId }) {
           <Button variant="light">
             <i className="bi bi-repeat"></i>
           </Button>
-          <Button variant="light">
-            <i className="bi bi-heart"></i> {likes}
+          <Button variant="light" onClick={handleLike}>
+            {isLiked ? (
+              <i className="bi bi-heart-fill text-danger"></i>
+            ) : (
+              <i className="bi bi-heart"></i>
+            )}
+            <span className="ms-1">{likes.length}</span>
           </Button>
           <Button variant="light">
             <i className="bi bi-graph-up"></i>
